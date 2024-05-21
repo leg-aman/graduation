05950fba68549ffc3b400b6a225d5dedeae8667c
leg-aman <126026416+leg-aman@users.noreply.github.com>
2024-05-21 18:22:38 -0400

    initialcommit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: [ '⚡️', '💥', '✨', '💫', '🌸'],
+    }).then(() => jsConfetti.addConfetti())
+})
+
+
+function run(){
+    jsConfetti.addConfetti({
+        emojis: [ '⚡️', '💥', '✨', '💫', '🌸','🎓'],
+    }).then(() => jsConfetti.addConfetti())
+}
